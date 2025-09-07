1b9a7a058a79c761fa364be4f4d575cda0c1f1e5
MAHDI <mhdiiranii@gmail.com>
2025-09-07 09:38:51 +0330

    framer

--- app/component/framer/framer.tsx
+++ app/component/framer/framer.tsx
@@ -1,24 +1,24 @@
 interface FrameType {
   height: string;
-  column: "one" | "two" | "three";
+  column?: number| null;
 }
 
 const Framer = ({ height, column }: FrameType) => {
   switch (column) {
-    case "one":
+    case 1:
       return (
         <div className={`w-full min-h-[${height}] rounded-2xl flex gap-4 p-4 `}>
           <div className="w-full  min-h-max rounded-2xl bg-grey-50 animate-pulse"></div>
         </div>
       );
-    case "two":
+    case 2:
       return (
         <div className={`w-full min-h-[${height}] rounded-2xl flex gap-4 p-4 `}>
           <div className="w-full  min-h-max rounded-2xl bg-grey-50 animate-pulse"></div>
           <div className="w-full  min-h-max rounded-2xl bg-grey-50 animate-pulse"></div>
         </div>
       );
-    case "three":
+    case 3:
       return (
         <div className={`w-full min-h-[${height}] rounded-2xl flex gap-4 p-4 `}>
           <div className="w-full min-h-max rounded-2xl bg-grey-50 animate-pulse"></div>
