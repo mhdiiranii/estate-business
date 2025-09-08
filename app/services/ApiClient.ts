e4945544aac4d5867b3f5e569ea1889036f761ea
MAHDI <mhdiiranii@gmail.com>
2025-09-08 10:56:33 +0330

    api

--- app/services/ApiClient.ts
+++ app/services/ApiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const ApiClient = () => {
   const AxiosRequest = axios.create({
-    baseURL: "http://localhost:3000/api/",
+    baseURL: "/api/",
     headers: { "Content-Type": "application/json" },
   });
 
